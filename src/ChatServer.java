e4678dbce8e4c4eba2700d5dc51edb90bc6a4853
deca <idrisadejumo@gmail.com>
2025-07-28 09:47:06 +0100

    initial commit

---
+++ src/ChatServer.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ChatServer {
+    private static Set<ClientHandler> clientHandlers = new HashSet<>();
+
+    public static void main(String[] args) {
+        final int PORT = 1234;
+    }
+}
