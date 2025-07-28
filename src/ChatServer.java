3abad5d674c1e5756bcd43675eae6b036fac1f68
deca <idrisadejumo@gmail.com>
2025-07-28 10:43:25 +0100

    Client handler

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,10 +1,25 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ChatServer {
     private static Set<ClientHandler> clientHandlers = new HashSet<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         final int PORT = 1234;
+        try (ServerSocket serverSocket = new ServerSocket(PORT)){
+            System.out.println("server started on port " + PORT);
+
+            while (true){
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected ");
+
+                ClientHandler clientHandler = new ClientHandler(clientSocket, clientHandlers);
+                clientHandlers.add(clientHandler);
+                new Thread(clientHandler).start();
+            }
+        }
     }
 }
--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,6 +1,56 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Set;
+
 public class ClientHandler implements Runnable{
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
+    private Set<ClientHandler> clients;
+
+    public ClientHandler(Socket socket, Set<ClientHandler> clients) throws IOException {
+        this.socket = socket;
+        this.clients = clients;
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true); // come back
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void  sendMessage(String msg){
+        writer.println(msg);
+    }
+
     @Override
     public void run() {
+        try {
+            String name = reader.readLine();
+            broadCast(name + ": new joine chat  ");
+            String msg ;
 
+            while ((msg = reader.readLine()) !=null){
+                broadCast( name + " :" + msg );
+            }
+        } catch (IOException e) {
+            System.out.println(  " client disconneted ");
+        }finally {
+            try {
+                socket.close();
+                clients.remove(this);
+                broadCast("A user has left the chat ");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    private void broadCast(String msg){
+        for (ClientHandler client : clients){
+            client.sendMessage(msg);
+        }
     }
 }
